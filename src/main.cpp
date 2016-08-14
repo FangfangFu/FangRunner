8a31e42c6da51884ac5653ab7eaf8f62f3f7d8db
Fangfang Fu <fangfang@fufangfang.com>
2016-08-14 12:24:22 -0500

    Update getting better performance

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,10 @@
 const int ROW = 20;
 const int COLUMN = 400;
 const Direction START_DIRECTION = Direction::NONE;
-const float START_POSITION_X = 10.0f;
+const float START_POSITION_X = 18.0f;
 const float START_POSITION_Y = 10.0f;
+const float LEFTBOUNDARY = 18.0f; // 45% boundary on both sides 
+const float RIGHTBOUNDARY = 22.0f; // Right boundary would become 55% of 40(the window width)
 // 1 unit is 16 pixel; 80x45
 int main()
 {
@@ -25,7 +27,7 @@ int main()
     // Declare the world
     World world(ROW, COLUMN, START_DIRECTION, START_POSITION_X, START_POSITION_Y);
     sf::Clock clock;
-
+    // Record last loop's x position
     float lastX = START_POSITION_X;
 
     while (window.isOpen())
@@ -74,10 +76,11 @@ int main()
         float originalX = world.GetPlayerX();
         float y = world.GetPlayerY();
 
-        if (x < 10.0f) {
-            x += 10 - x;
-        }else if (x > 30.0f) {
-            x -= x - 30;
+        
+        if (x < LEFTBOUNDARY) {
+            x += LEFTBOUNDARY - x;
+        }else if (x > RIGHTBOUNDARY) {
+            x -= x - RIGHTBOUNDARY;
         }else if (x < 0){
             x = 0;
         }
@@ -94,8 +97,10 @@ int main()
                 float squareGroundX = n;
                 if(worldMap[i][n] == 1){
                     squareGroundX += x - originalX;
-                    squareGround.setPosition(squareGroundX * 32.0f, (23.0f-i) * 32.0f);
-                    window.draw(squareGround);
+                    if (squareGroundX > -1.0f and squareGroundX < 40.0f){
+                        squareGround.setPosition(squareGroundX * 32.0f, (23.0f-i) * 32.0f);
+                        window.draw(squareGround);
+                    }
                 }
             }     
         }
